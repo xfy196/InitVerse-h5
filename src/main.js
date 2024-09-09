97055194f7dd8a10c61b1f4593ed9fed27ef314c
xufuying <xufuying@100doc.com.cn>
2024-09-09 22:23:06 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "amfe-flexible"
 import router from "./router";
-import "./style.css";
 import fastclick from "fastclick";
 import { createPinia } from "pinia";
 const pinia = createPinia();
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="text-red-500">home</div>
+  <div class="text-2xl text-red-500">home</div>
 </template>
 
 <script setup>
 </script>
 <style lang="scss" scoped>
-div{
-  font-size: 0.43rem;
-}
 </style>
