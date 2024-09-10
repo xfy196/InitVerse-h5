5871424d02ea62dea593b8a813f397e1e4aba302
许富荧 <xfy196@outlook.com>
2024-09-10 16:08:02 +0800

    update

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -22,8 +22,8 @@ onMounted(() => {
   height: 100vh;
   background: linear-gradient(180deg, purple 0%, #000000 100%);
   .main {
-    // height: calc(100vh - 133px - 126px);
-    height: 300px;
+    height: calc(100% - 133px - 126px);
+    // height: 300px;
     background-color: rgba(255, 255, 255, 0);
     padding-bottom: 16px;
     box-sizing: border-box;
