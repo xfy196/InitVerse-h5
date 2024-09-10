1e29512581c749c21188f68956b1488fd5b810d9
许富荧 <xfy196@outlook.com>
2024-09-10 16:48:22 +0800

    update

--- src/pages/transaction.vue
+++ src/pages/transaction.vue
@@ -1,10 +1,43 @@
 <template>
   <div>
     transaction
+    <div class="chart" ref="chartRef"></div>
   </div>
 </template>
 
 <script setup>
+import { onMounted, useTemplateRef, ref } from "vue";
+import * as echarts from "echarts";
+const chartRef = useTemplateRef("chartRef");
+let chart = null;
+onMounted(() => {
+  chart = echarts.init(chartRef.value);
+  const option = {
+    xAxis: {
+      data: ["2017-10-24", "2017-10-25", "2017-10-26", "2017-10-27"],
+    },
+    yAxis: {},
+    series: [
+      {
+        type: "candlestick",
+        data: [
+          [20, 34, 10, 38],
+          [40, 35, 30, 50],
+          [31, 38, 33, 44],
+          [38, 15, 5, 42],
+        ],
+      },
+    ],
+  };
+  chart.setOption(option);
+  window.addEventListener("resize", () => {
+    chart && chart.resize();
+  });
+});
 </script>
 <style lang="scss" scoped>
+.chart {
+  width: 100%;
+  height: 800px;
+}
 </style>
